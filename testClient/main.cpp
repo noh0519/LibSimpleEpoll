bfa910cbddf0320b5727f133aaa214d7ebc5a949
noh0519 <noh0519@hanmail.net>
2021-09-24 08:42:09 +0000

    modify testcode

--- testClient/main.cpp
+++ testClient/main.cpp
@@ -1,6 +1,110 @@
-#include <stdio.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+
+#define BUFF_SIZE 1024
+#define EPOLL_SIZE 1024
+
+int g_step = 0;
 
 int main(int argc, char **argv) {
-  printf("Start Client\n");
+  // socket variable
+  int client_socket = -1;
+  struct sockaddr_in server_addr;
+
+  char buff_recv[4096];
+  char buff_send[4096];
+  // ~socket variable
+
+  // epoll variables
+  int epoll_fd = -1;
+  struct epoll_event client_event;
+  struct epoll_event *events;
+  // ~epoll variables
+
+  client_socket = socket(PF_INET, SOCK_STREAM, 0);
+  if (client_socket == -1) {
+    printf("socket create fail\n");
+    exit(1);
+  }
+
+  memset(&server_addr, 0x00, sizeof(sockaddr_in));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(4000);
+  server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    printf("socket connect fail\n");
+    exit(1);
+  }
+
+  /* epoll create */
+  if ((epoll_fd = epoll_create1(0)) == -1) {
+    printf("epoll create fail\n");
+    exit(1);
+  }
+
+  /**/
+  client_event.events = EPOLLIN | EPOLLOUT;
+  client_event.data.fd = client_socket;
+  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &client_event);
+
+  /* alloc event */
+  events = (struct epoll_event *)malloc(sizeof(struct epoll_event) * EPOLL_SIZE);
+
+  /* first send*/
+  memset(buff_send, 0x00, 4096);
+  snprintf(buff_send, 4096, "%s", "start step 0");
+  send(client_socket, buff_send, 4096, 0);
+
+  while (1) {
+    int event_count = epoll_wait(epoll_fd, events, EPOLL_SIZE, -1);
+
+    if (event_count == -1) {
+      printf("epoll wait fail\n");
+      exit(1);
+    }
+
+    for (int i = 0; i < event_count; i++) {
+      int who = events[i].data.fd;
+      uint32_t what = events[i].events;
+
+      if (who == client_socket) {
+        if (what & EPOLLIN) {
+          memset(buff_recv, 0x00, 4096);
+          recv(who, buff_recv, 4096, 0);
+          if (!strcmp("start step 1", buff_recv)) {
+            printf("step 1 get\n");
+            g_step = 2;
+          } else if (!strcmp("start step 3", buff_recv)) {
+            printf("step 3 get\n");
+            g_step = 4;
+          } else if (!strcmp("start step 5", buff_recv)) {
+            printf("step 5 get\n");
+            g_step = 6;
+          }
+        } else if (what & EPOLLOUT) {
+          memset(buff_send, 0x00, 4096);
+          if (g_step == 2) {
+            snprintf(buff_send, 4096, "%s", "start step 2");
+            send(who, buff_send, 4096, 0);
+            g_step = 3;
+          } else if (g_step == 4) {
+            snprintf(buff_send, 4096, "%s", "start step 4");
+            send(who, buff_send, 4096, 0);
+            g_step = 5;
+          } else if (g_step == 6) {
+            printf("login completed\n");
+            struct epoll_event event;
+            event.events = EPOLLIN;
+            event.data.fd = who;
+            epoll_ctl(epoll_fd, EPOLL_CTL_MOD, who, &event);
+          }
+        }
+      }
+    }
+  }
+
   return 0;
 }
--- testServer/main.cpp
+++ testServer/main.cpp
@@ -10,44 +10,59 @@ public:
   void setFD(int fd) { m_fd = fd; }
   int getFD() { return m_fd; }
 
+  void setStep(int step) { m_step = step; }
   int getStep() { return m_step; }
 
-  // void loginReadCallback(int fd, short what, void *arg) { static_cast<MyFDClass *>(arg)->loginReadFunc(fd, what); }
   void loginReadFunc(int fd, short what) {
-    if (what | EPOLLIN) {
+    if (what & EPOLLIN) {
       memset(buff_recv, 0x00, 4096);
       // int ref = recv(fd, buff_recv, 4096, 0);
       recv(fd, buff_recv, 4096, 0);
-      if (!strcmp("start step 1", buff_recv)) {
-        m_step = 1;
+      if (!strcmp("start step 0", buff_recv)) {
+        printf("step 0 get\n");
+        setStep(1);
       } else if (!strcmp("start step 2", buff_recv)) {
-        m_step = 2;
-      } else if (!strcmp("start step 3", buff_recv)) {
-        m_step = 3;
+        printf("step 2 get\n");
+        setStep(3);
+      } else if (!strcmp("start step 4", buff_recv)) {
+        printf("step 4 get\n");
+        setStep(5);
       }
     }
   }
-  // void loginWriteCallback(int fd, short what, void *arg) { static_cast<MyFDClass *>(arg)->loginWriteFunc(fd, what); }
+
   void loginWriteFunc(int fd, short what) {
-    if (what | EPOLLOUT) {
+    if (what & EPOLLOUT) {
       memset(buff_send, 0x00, 4096);
       if (m_step == 1) {
-        snprintf(buff_send, 4096, "%s", "end step 1");
+        snprintf(buff_send, 4096, "%s", "start step 1");
         send(fd, buff_send, 4096, 0);
-      } else if (m_step == 2) {
-        snprintf(buff_send, 4096, "%s", "end step 2");
-        send(fd, buff_send, 4096, 0);
+        setStep(2);
       } else if (m_step == 3) {
-        snprintf(buff_send, 4096, "%s", "end step 3");
+        snprintf(buff_send, 4096, "%s", "start step 3");
         send(fd, buff_send, 4096, 0);
+        setStep(4);
+      } else if (m_step == 5) {
+        snprintf(buff_send, 4096, "%s", "start step 5");
+        send(fd, buff_send, 4096, 0);
+        setStep(6);
+        printf("login completed\n");
       }
+    }
+  }
+
+  void runReadFunc(int fd, short what) {
+    if (what & EPOLLIN) {
+      memset(buff_recv, 0x00, 4096);
+      recv(fd, buff_recv, 4096, 0);
+      printf("get data(%d) : %s\n", fd, buff_recv);
     }
   }
 
 private:
   int m_fd = -1;
 
-  int m_step = 0; // 0 : init, 1 ~ 3 : step
+  int m_step = 0; // 0 : init, 1 ~ 3 : login step, 4 : running
 
   char buff_recv[4096] = {0};
   char buff_send[4096] = {0};
@@ -71,16 +86,22 @@ int main(int argc, char **argv) {
   mysepoll.init(SEPOLL_TYPE::ACCEPT, 1024, 4096, 4000, "127.0.0.1");
   mysepoll.setInitReadFunc([](int fd, short what, void *arg) -> void { static_cast<MyFDClass *>(arg)->loginReadFunc(fd, what); },
                            EPOLLIN | EPOLLHUP | EPOLLRDHUP | EPOLLERR);
-  // mysepoll.setInitWriteFunc(^MyFDClass);
+  mysepoll.setInitWriteFunc([](int fd, short what, void *arg) -> void { static_cast<MyFDClass *>(arg)->loginWriteFunc(fd, what); },
+                            EPOLLOUT);
 
   std::thread tsepoll(&SEpoll<MyFDClass, FDSetFunc, FDGetFunc>::run, &mysepoll);
   tsepoll.detach();
 
+  int loop_cnt = 0;
   while (1) {
-    // printf("client count %lu\n", mfcs->size());
     for (auto a : *mfcs) {
       if (a->getStep() == 3) {
         printf("find step 3 (%d)\n", a->getFD());
+        a->setStep(4);
+        mysepoll.setReadFunc(
+            a->getFD(), [](int fd, short what, void *arg) -> void { static_cast<MyFDClass *>(arg)->runReadFunc(fd, what); }, a.get(),
+            EPOLLIN | EPOLLHUP | EPOLLRDHUP | EPOLLERR);
+        mysepoll.unsetWriteFunc(a->getFD());
       }
     }
     std::this_thread::sleep_for(std::chrono::seconds(1));
