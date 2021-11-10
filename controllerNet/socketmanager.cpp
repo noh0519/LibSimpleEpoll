9d4cf6a91b0b6e957dd2ab2507643286c385c911
noh0519 <noh0519@hanmail.net>
2021-11-10 08:56:24 +0000

    정책 dump 로그 주석

--- controllerNet/socketmanager.cpp
+++ controllerNet/socketmanager.cpp
@@ -715,7 +715,7 @@ void SocketManager::flushConfigData(SetConfigList setcfg) {
     _rogue_clients.clear();
   } break;
   case SetConfigList::POLICY_HASH: {
-    std::cout << _threat_policy.dump(4) << std::endl;
+    // std::cout << _threat_policy.dump(4) << std::endl;
     SmartIO io_policy("set", "ipc:///tmp/policy_set.uds");
     io_policy.set(_threat_policy, nlohmann::json({}));
     _threat_policy.clear();
